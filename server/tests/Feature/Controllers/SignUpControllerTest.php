6eb877ae29828701b4ce94e5c15e69538c2844ee
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-02-02 08:35:01 +0900

    User create validation

--- server/tests/Feature/Controllers/SignUpControllerTest.php
+++ server/tests/Feature/Controllers/SignUpControllerTest.php
@@ -42,4 +42,31 @@ class SignUpControllerTest extends TestCase
 
         $this->assertTrue(\Hash::check('abcd1234', $user->password));
     }
+
+    /** @test store */
+    function 不正なデータではユーザー登録できない()
+    {
+        //$this->withoutExceptionHandling();
+        $url = 'signup';
+
+        $this->from('signup')->post($url, [])
+            ->assertRedirect('signup');
+
+//        app()->setlocale('testing');
+//
+//        $this->post($url, ['name' => ''])->assertSessionHasErrors(['name' => 'required']);
+//        $this->post($url, ['name' =>str_repeat('あ', 21)])->assertSessionHasErrors(['name' => 'max']);
+//        $this->post($url, ['name' =>str_repeat('あ', 20)])->assertSessionDoesntHaveErrors('name');
+//
+//        $this->post($url, ['email' => ''])->assertSessionHasErrors(['email' => 'required']);
+//        $this->post($url, ['email' => 'aa@bb@cc'])->assertSessionHasErrors(['email' => 'email']);
+//        $this->post($url, ['email' => 'aa@ああ.いい'])->assertSessionHasErrors(['email' => 'email']);
+//
+//        User::factory()->create(['email' => 'aaa@bbb.net']);
+//        $this->post($url, ['email' => 'aaa@bbb.net'])->assertSessionHasErrors(['email' => 'unique']);
+//
+//        $this->post($url, ['password' => ''])->assertSessionHasErrors(['password' => 'required']);
+//        $this->post($url, ['password' => 'abcd123'])->assertSessionHasErrors(['password' => 'min']);
+//        $this->post($url, ['password' => 'abcd1234'])->assertSessionDoesntHaveErrors('password');
+    }
 }
